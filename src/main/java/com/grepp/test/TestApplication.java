91afed4f3657f3c4b4adb9943937c32ce9c79105
sunghyun <20190171@inu.ac.kr>
2024-09-12 11:48:09 +0900

    first

--- src/main/java/com/grepp/test/TestApplication.java
+++ src/main/java/com/grepp/test/TestApplication.java
@@ -9,6 +9,7 @@ public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
         System.out.println("issue 1");
+        System.out.println("first 에서 온 issue");
     }
 
 }
